9d1730d41babc86d86c1a485eccffc9449615a5f
gaut-b <53061088+gaut-b@users.noreply.github.com>
2026-05-26 20:50:13 +0200

    feat: sign build with keystore (#24)
    * feat: sign build with keystore
    
    * fix: comments
    
    * workflow: use jks file rather than .keystore
    
    * add env vars
    
    * Revert "add env vars"
    
    This reverts commit 26c13db7aea65c7fa9fe5477b77210988515bde5.
    
    * added keystore variables to workflow
    
    * fix workflow
    
    * add bundle generation and signing
    
    * generate bundles from development env
    
    * fix staging/development
    
    ---------
    
    Co-authored-by: Clement Viel <cviel1@ford.com>

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -69,7 +69,8 @@ export type InputControllerType<T extends FieldValues> = {
 };
 
 interface ControlledInputProps<T extends FieldValues>
-  extends NInputProps, InputControllerType<T> {}
+  extends NInputProps,
+    InputControllerType<T> {}
 
 export const Input = React.forwardRef<NTextInput, NInputProps>((props, ref) => {
   const { label, error, testID, rightElement, ...inputProps } = props;
